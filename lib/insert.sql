eefc312faf8e621005d2b80b45e037b24270adb2
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-09-09 03:38:52 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,8 @@
 INSERT INTO series (title) VALUES
-('lord of the rings'), ('northern lights')
+('lord of the rings'), ('northern lights');
+
+INSERT INTO books (title) values
+('hobbit 1'), ('hobbit 2'), ('hobbit 3'),
+('fellowship of the rings'),
+('two towers'),
+('return of the king');
