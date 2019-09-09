60f8950334453e6b7ae1f83858290315faa65d4d
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-09-09 03:30:33 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -4,3 +4,13 @@ CREATE TABLE series(
   author_id INTEGER,
   subgenre_id INTEGER
 )
+
+CREATE TABLE subgenres(
+  name TEXT,
+  id INTEGER PRIMARY KEY,
+)
+
+CREATE TABLE authors(
+  name TEXT,
+  id INTEGER PRIMARY KEY
+)
