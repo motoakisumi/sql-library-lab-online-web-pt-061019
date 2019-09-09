90211d957d3f815aad491f4324f5ae725d2fd31a
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-09-09 05:08:34 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -63,7 +63,6 @@ def select_character_names_and_number_of_books_they_are_in
   ON characters.id = character_books.character_id
   GROUP BY characters.name
   ORDER BY value_occurrence DESC
-  ORDER BY characters.name
   ;
   "
 end
