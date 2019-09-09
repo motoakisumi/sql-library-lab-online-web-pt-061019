65d2ece116712d820b34ee9269284454e2f6c2d4
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-09-09 03:34:03 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -3,24 +3,24 @@ CREATE TABLE series(
   id INTEGER PRIMARY KEY,
   author_id INTEGER,
   subgenre_id INTEGER
-)
+);
 
 CREATE TABLE subgenres(
   name TEXT,
   id INTEGER PRIMARY KEY
-)
+);
 
 CREATE TABLE authors(
   name TEXT,
   id INTEGER PRIMARY KEY
-)
+);
 
 CREATE TABLE books(
   title TEXT,
   year INTEGER
   series_id INTEGER,
   id INTEGER PRIMARY KEY
-)
+);
 
 CREATE TABLE characters(
   name TEXT,
@@ -28,10 +28,10 @@ CREATE TABLE characters(
   motto TEXT,
   author_id INTEGER,
   id INTEGER PRIMARY KEY
-)
+);
 
 CREATE TABLE character_books(
   id INTEGER PRIMARY KEY,
   book_id INTEGER,
   character_id INTEGER
-)
+);
